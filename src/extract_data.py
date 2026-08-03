adea72faae6d607c903b7b7e3aa61c7ba49f4013
teubragaa <teubraga2002.gmail.com>
2026-08-02 23:41:03 -0300

    feat: implement weather data analysis

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,15 +1,15 @@
-import requests
-import json
-from pathlib import Path
+import requests  # Biblioteca para fazer requisições HTTP
+import json  # Biblioteca para trabalhar com arquivos JSON
+from pathlib import Path # Biblioteca para manipular caminhos de arquivos
 
-import logging  
+import logging # Biblioteca para registrar mensagens do programa
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s') 
 
-def extract_data(url: str) -> list:
-    response = requests.get(url)
-    data = response.json()
+def extract_data(url: str) -> list: # Função que recebe uma URL e retorna os dados
+    response = requests.get(url) # Faz uma requisição para a URL
+    data = response.json() # Converte a resposta para formato JSON (dicionário)
 
-    if response.status_code != 200:
+    if response.status_code != 200:   # Verifica se a requisição deu erro
         logging.error(f"Erro na requisicao")
         return []   
 
@@ -17,12 +17,17 @@ def extract_data(url: str) -> list:
         logging.warning(f"No data encontrado.")
         return []   
 
-    output_path = 'data/weather_data.json'
-    output_dir = Path(output_path).parent
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    with open(output_path, 'w') as f:
-        json.dump(data, f, indent=4)
+    output_path = 'data/weather_data.json'  # Caminho onde o arquivo será salvo
+    output_dir = Path(output_path).parent  # Pega apenas a pasta ("data")
+    output_dir.mkdir(
+        parents=True,     # Cria pastas intermediárias se necessário
+        exist_ok=True)    # Não gera erro se a pasta já existir
+ 
+    with open(output_path, 'w') as f:  # Abre o arquivo para escrita 'w'
+        json.dump(
+            data,           # Dados que serão gravados
+            f,              # Arquivo onde serão gravados
+            indent=4)        # Deixa o JSON formatado (mais bonito)
 
     logging.info(f"Data successfully extracted and saved to {output_path}.")
     return data
